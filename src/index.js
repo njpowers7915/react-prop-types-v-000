35a39cb2530c1dcbea3070b4fc1abd0d4484318d
njpowers7915 <njpowers7915@gmail.com>
2018-09-11 09:36:13 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 // Code Goes Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Order extends React.Component {
+
+  render() {
+    // ...
+  }
+}
+
+Order.defaultProps = {
+  cone: true,
+  size: 'regular'
+};
+
+Order.propTypes = {};
